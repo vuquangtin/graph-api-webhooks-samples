c6863d842a03e611925262b18f07c8d667fdbbed
Doan Hong Quang <hongquangdoan@gmail.com>
2017-04-15 18:02:17 +0700

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -51,8 +51,14 @@ app.post('/facebook', function(req, res) {
     // recommend sending 401 status in production for non-validated signatures
     // res.sendStatus(401);
   }
+  
+  // { entry: [ { changes: [Object], id: '177451742755395', time: 1492253900 } ],
   console.log(req.body);
-   console.log('.............................................1');
+  console.log('.............................................1');
+  
+  console.log(req.body.entry);
+  console.log(req.body.entry.time);
+  console.log(req.body.entry.id);
   console.log(req.body.entry.changes);
   console.log('..............................................2');
   console.log('finish quangdh0');
